20ed8c2d1dcde07fe96034fa8049246c93fe28ac
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-04-17 23:08:45 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,7 +2,8 @@ import React from "react";
 import { TshirtList } from "./TshirtList";
 import { Cart } from "./Cart";
 import { CartProvider } from "./CartContext";
-
+import {navbar} from "./navbar"
+import {cards} from "./cards"
 //create your first component
 export function App() {
 	return (
